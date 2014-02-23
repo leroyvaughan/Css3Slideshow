6fd7d2b0e67a9e86c5ab4706d3d0233d28313983
Leroy Vaughan <leroy@glvconsulting.com>
2014-02-23 02:07:07 -0800

    Phase 2 Update
    ADDED:
    - images
    - some css
    
    No buttons yet.  Still works in IE8.
    Native JavaScript Only.

---
+++ js/css3Slideshow.js
@@ -0,0 +1,40 @@
+﻿
+function slideShow (pDiv) {
+    var showTimer;
+    var childElemCnt = 0, slides = [];
+    var ieConsole = document.getElementById("myConsole");
+
+
+    /* get actual number of child elements because 
+        pDiv.childElementCount does not work inIE8 */
+    var collection = pDiv.children;
+    for (var x = 0; x < collection.length; x++) {
+        curElem = collection[x];
+        if (curElem.tagName == "DIV") {
+            slides.push(curElem);
+            childElemCnt++;
+        }
+    }
+    //ieConsole.innerHTML = childElemCnt;
+
+
+    var ix = 0, curObj, lastObj;
+
+    showTimer = setInterval(function () {
+        if (ix > 0){
+            lastObj = curObj;
+            lastObj.style.left = '900px';
+        }
+
+        curObj = slides[ix];
+        curObj.style.left = '25px';
+        ix++;
+
+        if (ix >= childElemCnt) {
+            clearInterval(showTimer);
+        }
+
+    }, 2000);
+
+}
+slideShow(document.getElementById("slideWrap"));
